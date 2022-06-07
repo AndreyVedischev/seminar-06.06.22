c2fd0fd9b7b383aa95aaa3dd91a9be14c3f16917
Andrey Vedischev <vedischev76@mail.ru>
2022-06-07 22:47:17 +0500

    добавили исправление

--- задача002/Program.cs
+++ задача002/Program.cs
@@ -2,6 +2,10 @@
 Console.Write("Введите номер дня недели: ");
 int day = Convert.ToInt32(Console.ReadLine());
 
+if ((day < 1)||(day > 7)){ 
+    Console.WriteLine("Ошибка!");
+    return;
+}
 if (day == 1) Console.WriteLine("Понедельник");
 if (day == 2) Console.WriteLine("Вторник");
 if (day == 3) Console.WriteLine("Среда");
@@ -9,4 +13,4 @@ if (day == 4) Console.WriteLine("Четверг");
 if (day == 5) Console.WriteLine("Пятница");
 if (day == 6) Console.WriteLine("Суббота");
 if (day == 7) Console.WriteLine("Воскресенье");
-else(1 < day > 7) Console.WriteLine("Ошибка!");
+
