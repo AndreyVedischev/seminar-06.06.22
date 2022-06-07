6e65bebe53754da44836c469e68e1df6bef55209
Andrey Vedischev <vedischev76@mail.ru>
2022-06-07 23:24:17 +0500

    добавили пример возведения числа в степень

---
+++ пример001/Program.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+
+Console.Clear();
+
+int number = Convert.ToInt32(Console.ReadLine());
+//int number1 = int.Parse(Console.ReadLine());
+
+int sqrNumber = number*number;
+//int sqrNumber1 = Convert.ToInt32(Math.Pow(number1,2));
+
+Console.WriteLine(sqrNumber);
+//Console.WriteLine(sqrNumber1);
