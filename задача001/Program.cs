8207201ea078581e04621e04522bf44eede6acdc
Andrey Vedischev <vedischev76@mail.ru>
2022-06-07 22:16:39 +0500

    добавили задачу квадрат числа

---
+++ задача001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if((a*a) == b) Console.WriteLine("Число b является квадратом числа а.");
+else           Console.WriteLine("Число b не является квадратом числа а.");
