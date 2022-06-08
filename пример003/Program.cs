4a514d30bed9c96234a623c9aeeb1bc06a055f20
Andrey Vedischev <vedischev76@mail.ru>
2022-06-08 22:55:11 +0500

    добавили проверку на трехзначное число и принудительно сделали его положительным

--- пример003/Program.cs
+++ пример003/Program.cs
@@ -2,4 +2,9 @@
 Console.Write("Введите трехзначное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
+if((Math.Abs(N) < 100) ||(Math.Abs(N)>999)){
+    Console.WriteLine("Введено не трехзначное число!!!");
+    return;
+}
+
 Console.WriteLine(N%10);
